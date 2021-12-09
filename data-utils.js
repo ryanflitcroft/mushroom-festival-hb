2f2529a1149c949b76f7cb337343617f467851fa
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-09 11:28:51 -0800

    adjusted findFriendByName function to return obj vs obj property value, test written and passing

--- data-utils.js
+++ data-utils.js
@@ -1,5 +1,9 @@
-// export default function findFriendByName(name, friends) {
-//     // iterate through the friends
-    
-//     // when you find a friend with the correct name, return that friend
-// }
+export function findFriendByName(name, friends) {
+// iterate through the friends
+    for (let friend of friends) {
+        if (friend.name === name) {
+            return friend;
+        }
+    }
+// when you find a friend with the correct name, return that friend
+}
